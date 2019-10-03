8132d8ab9a14dc8fe1d5fa74bc57703a80acf470
jiacheng2018 <penggysosmart@gmail.com>
2019-10-03 20:50:47 +1000

    tweak google strategy

--- config/prod.js
+++ config/prod.js
@@ -3,5 +3,5 @@ module.exports={
     googleClientSecret:process.env.GOOGLE_CLIENT_SECRET,
     mongoURI:process.env.MONGO_URI,
     cookiekey:process.env.COOKIE_KEY,
-    googleRedirectURI:'https://damp-meadow-12226.herokuapp.com'
+    // googleRedirectURI:'https://damp-meadow-12226.herokuapp.com'
 };
